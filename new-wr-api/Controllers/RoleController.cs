f04a0eb4407884f63c1b75daea4276a01c7b85b3
githubnguyendang <73978541+githubnguyendang@users.noreply.github.com>
2023-03-03 14:20:54 +0700

    Add project files.

---
+++ new-wr-api/Controllers/RoleController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using new_wr_api.Data;
+using new_wr_api.Models;
+using new_wr_api.Repositories;
+
+namespace new_wr_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleRepository _repo;
+
+        public RoleController(RoleRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        [Route("roles")]
+        //[Authorize(Roles = "admin")]
+        public async Task<List<ApplicationRole>> GetAllRoles()
+        {
+            return await _repo.GetAllRolesAsync();
+        }
+
+        [HttpGet]
+        [Route("role/{roleId}")]
+        //[Authorize(Roles = "admin")]
+        public async Task<ApplicationRole?> GetRoleById(string roleId)
+        {
+            return await _repo.GetRoleByIdAsync(roleId);
+        }
+
+        [HttpPost]
+        [Route ("create-role")]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult<ApplicationRole>> CreateRole(RoleViewModel model)
+        {
+            var res = await _repo.CreateRoleAsync(model.roleName, model.isDefault);
+            if (res.Succeeded)
+            {
+                return Ok(new
+                {
+                    message = "Role is created",
+                    res = res
+                });
+            }
+            return BadRequest(new
+            {
+                message = res
+            });
+        }
+
+        [HttpPost]
+        [Route("detete-role/{roleId}")]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult<ApplicationRole>> DeleteRole(string roleId)
+        {
+            var res = await _repo.DeleteRoleAsync(roleId);
+            if (res == false)
+            {
+                return BadRequest(new
+                {
+                    message = res
+                });
+            }
+            return Ok(new
+            {
+                message = "Role is deleted."
+            });
+        }
+    }
+}
---
+++ new-wr-api/Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace new_wr_api.Data
+{
+    public class DatabaseContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        private readonly IConfiguration configuration;
+        public DatabaseContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+        //
+        //public DbSet<ApplicationRole>? Roles { get; set; }
+    }
+}
